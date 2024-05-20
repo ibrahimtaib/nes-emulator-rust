02a923fd490d1bc739c650a86f21c8fc55b7b392
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2024-05-20 14:01:59 +0200

    Adding cpu skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,104 @@
-fn main() {
-    println!("Hello, world!");
+struct CPU {
+    pc: u16,
+    sp: u8,
+    a: u8,
+    x: u8,
+    y: u8,
+    status: u8,
+    memory: [u8; 0x10000],
 }
+
+impl CPU {
+    fn new() -> Self {
+        CPU {
+           pc: 0,
+           sp: 0,
+           a: 0,
+           x: 0,
+           y: 0,
+           status: 0,
+           memory: [0; 0x10000], 
+        }
+    }
+
+    fn read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn write(&mut self, addr: u16, value: u8) {
+        (self).memory[addr as usize] = value; 
+    }
+
+    fn fetch_next_pc(&mut self) -> u8 {
+        let next_instruction: u16 = self.pc;
+        self.pc += 1;
+        self.memory[next_instruction as usize]
+    }
+
+    fn get_addressing_mode(&mut self) {
+    }
+
+    fn decode_and_execute(&mut self, opcode: u8) {
+        match opcode {
+           0xA9 => {
+               let param = self.fetch_next_pc();
+               self.a = param;
+           }
+           _ => todo!(),
+        } 
+    }
+
+    fn run(&mut self) {
+        loop {
+            let opcode = self.fetch_next_pc();
+            self.decode_and_execute(opcode);
+            // Implement interrupt handling and cycle management
+        }
+    }
+}
+
+fn main() {
+    let mut cpu = CPU::new();
+    cpu.run();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cpu_initialization() {
+        let cpu = CPU::new();
+        assert_eq!(cpu.pc, 0);
+        assert_eq!(cpu.sp, 0);
+        assert_eq!(cpu.a, 0);
+        assert_eq!(cpu.x, 0);
+        assert_eq!(cpu.y, 0);
+        assert_eq!(cpu.status, 0);
+        assert_eq!(cpu.memory[0], 0);
+    }
+
+    #[test]
+    fn test_memory_read_write() {
+        let mut cpu = CPU::new();
+        cpu.write(0x1234, 0x56);
+        assert_eq!(cpu.read(0x1234), 0x56);
+    }
+
+    #[test]
+    fn test_fetch_opcode() {
+        let mut cpu = CPU::new();
+        cpu.memory[0x0000] = 0xA9; // LDA immediate opcode
+        assert_eq!(cpu.fetch_next_pc(), 0xA9);
+        assert_eq!(cpu.pc, 1);
+    }
+
+    #[test]
+    fn test_lda_immediate() {
+        let mut cpu = CPU::new();
+        cpu.write(cpu.pc , 0x06);
+        cpu.decode_and_execute(0xA9);
+        assert_eq!(cpu.a, 0x06);
+    }
+}
+
